8c1a82b67a75dc1742935ae2bab85c33ded56488
fitnessbg <72287887+fitnessbg@users.noreply.github.com>
2026-03-14 21:51:11 +1100

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const BOT_CONFIG = {
+  PROJECT_NAME:  "OILFIRE",
+  TOKEN_SYMBOL:  "OLF",
+  TOKEN_ADDRESS: "0x...",
+  WEBSITE_URL:   "",
+  WHITEPAPER_URL:"",
+  TWITTER_URL:   "https://twitter.com/oilfiretoken",
+  PROMO_CHANNELS: [],
+  ADMIN_IDS:      [],
+  COINGECKO_ID:  "",
+};
